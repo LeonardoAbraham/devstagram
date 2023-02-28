69a8db7844d80abfaf2c0f19bc11652c2411032c
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-02-28 15:24:29 -0600

    Autenticar un Usuario que ha creado su cuenta

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -37,6 +37,17 @@ class RegisterController extends Controller
             'password'=> Hash::make($request->password)
         ]);
 
+        //Autenticar un usuario
+        //auth()->attempt([
+        //    'email' => $request->email,
+        //    'password' => $request->password
+        //]);
+
+        //Otra forma de autenticar
+        auth()->attempt($request->only('email','password'));
+
+        return redirect()->route('posts.index');
+
 
     }
 }
