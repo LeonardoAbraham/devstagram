66311b5c52d4ff481fbfcc1d640dd2464c4a75a5
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-03-06 15:49:26 -0600

    Creando Routing para almacenar imágenes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
+        @stack('styles')
 
         <title>DevStagram - @yield('titulo')</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
