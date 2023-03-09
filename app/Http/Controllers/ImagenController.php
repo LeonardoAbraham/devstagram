8e297356dcac40012f73b8c3f14c20db4810f85c
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-03-09 12:13:57 -0600

    Almacenando las imágenes en el servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -10,6 +12,14 @@ class ImagenController extends Controller
     {
         $imagen = $request->file('file');
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);//recorte de imagen a 1000x1000 pixeles
+
+        $imagenPath = public_path('uploads') . "/" . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
