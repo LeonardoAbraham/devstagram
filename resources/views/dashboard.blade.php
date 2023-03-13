76cc5b00edb459aaf5fd41e5cbe813b3d0221993
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-03-13 15:01:15 -0600

    Mostrar mensaje de forma condicional

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,16 +35,20 @@
         </h2>
 
 
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="">
-                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }} ">
-                    </a>
-                </div>
-            @endforeach
+        @if ($posts->count())
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                @foreach ($posts as $post)
+                    <div>
+                        <a href="">
+                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }} ">
+                        </a>
+                    </div>
+                @endforeach
 
-        </div>
+            </div>
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
+        @endif
 
 
     </section>
