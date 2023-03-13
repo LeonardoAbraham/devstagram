878fd169b9fcaffa767a5f6ac5a19055779518c3
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-03-13 14:54:35 -0600

    Mostrando las publicaciones @foreach

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import FullReload from 'vite-plugin-full-reload';
-
 export default defineConfig({
     server:{
         hmr: {
@@ -9,7 +7,6 @@ export default defineConfig({
         },
     },
     plugins: [
-        FullReload(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
