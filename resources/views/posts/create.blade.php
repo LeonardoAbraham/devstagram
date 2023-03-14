b69c9fd0c1d8be82672276592454d5bf1e277c59
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-03-14 08:58:22 -0600

    Posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -52,9 +52,7 @@
                         name="descripcion"
                         placeholder="Descripción de la Publicación"
                         class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror"
-                    >
-                        {{ old('descripcion') }}
-                    </textarea>
+                    >{{ old('descripcion') }}</textarea>
                     @error('descripcion')
                         <p class=" bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}
