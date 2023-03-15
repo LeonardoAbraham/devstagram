0489ecb473a4ba64a948774e7a47cca842ce2e0b
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-03-15 13:16:17 -0600

    Añadir el Routing para guardar comentario

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    //
+    public function store()
+    {
+       //validar
+
+
+       //almacenar el resultado
+
+
+       //Imprimir un mensaje
+    }
 }
